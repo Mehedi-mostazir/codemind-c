96fde91ee517ae926e212d30f5129f42a4ef51cb
Mehedi Mostazir <97426129+Mehedi-mostazir@users.noreply.github.com>
2022-06-29 23:21:45 +0530

    #include<stdio.h> #include<math.h> int main() {     int g,d;     scanf("%d",&g);     for(d=1;d<=g;d++)     {     int n,temp,count,count1,i,j,k,m,s,x,y,r,f;     scanf("%d",&n);     for(i=n;i>=0;i--)     {             count=0;             for(j=1;j<=i;j++)             {                 if(i%j==0)                 {                     count++;                 }             }             if(count==2)             {                 x=i;                 s=1;                 break;             }             else             {                 continue;             }     }     k=n;     while(k>0)     {         count1=0;         for(i=1;i<=k;i++)         {             if(k%i==0)             {                 count1++;             }         }         if(count1==2)         {             y=k;             m=1;             break;         }         else         {             k++;         }     }     f=abs(n-x);     r=abs(n-y);     if(f<r || f==r)     {         printf("%d ",x);     }     else     {         printf("%d ",y);     }     } }

--- Nearest_Prime.c
+++ Nearest_Prime.c
@@ -1,52 +1,67 @@
-#include<stdio.h>
-void fun(int a)
-{
-    int left=0,l,r,right=0,fact=0;
-    for(int i=a;left==0;i--)
-    {
-        fact=0;
-        for(int j=1;j<=i/2;j++)
-        {
-            if(i%j==0)
-            fact++;
-        }
-        if(fact==1)
-        {
-            left=i;
-            l=a-left;
-            break;
-        }
-    }
-    for(int i=a;right==0;i++)
-    {
-        fact=0;
-        for(int j=1;j<=i/2;j++)
-        {
-            if(i%j==0)
-            fact++;
-        }
-        if(fact==1)
-        {
-            right=i;
-            r=right-a;
-            break;
-        }
-    }
-    if(r>l || r==l)
-    printf("%d
-",left);
-    else if( r<l )
-    printf("%d
-",right);
-}
+ #include<stdio.h>
+#include<math.h>
 int main()
 {
-    int n,x;
-    scanf("%d",&n);
-    while(n)
+    int g,d;
+    scanf("%d",&g);
+    for(d=1;d<=g;d++)
     {
-        scanf("%d",&x);
-        fun(x);
-        n--;
+    int n,temp,count,count1,i,j,k,m,s,x,y,r,f;
+    scanf("%d",&n);
+    for(i=n;i>=0;i--)
+    {
+            count=0;
+            for(j=1;j<=i;j++)
+            {
+                if(i%j==0)
+                {
+                    count++;
+                }
+            }
+            if(count==2)
+            {
+                x=i;
+                s=1;
+                break;
+            }
+            else
+            {
+                continue;
+            }
+    }
+    k=n;
+    while(k>0)
+    {
+        count1=0;
+        for(i=1;i<=k;i++)
+        {
+            if(k%i==0)
+            {
+                count1++;
+            }
+        }
+        if(count1==2)
+        {
+            y=k;
+            m=1;
+            break;
+        }
+        else
+        {
+            k++;
+        }
+    }
+    f=abs(n-x);
+    r=abs(n-y);
+    if(f<r || f==r)
+    {
+        printf("%d
+",x);
+    }
+    else
+    {
+        printf("%d
+",y);
+    }
     }
 }
